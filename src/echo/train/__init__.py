da6cf5aaf5c3c18e07b056a728bd830808739e62
nsschw <schwager@uni-trier.de>
2025-11-06 15:45:01 +0000

    init

---
+++ src/echo/train/__init__.py
@@ -0,0 +1,10 @@
+from echo.train.config import ExperimentConfig, TrainingConfig, ModelConfig, DataConfig
+from echo.train.train import train
+
+__all__ = [
+    "ExperimentConfig",
+    "TrainingConfig",
+    "ModelConfig",
+    "DataConfig",
+    "train"
+]
---
+++ src/echo/utility/__init__.py
@@ -0,0 +1,3 @@
+from echo.utility.llm import LLM
+
+__all__ = ["LLM"]
