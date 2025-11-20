d319927351513bff8459f256ed23d03e7be71e5c
nsschw <schwager@uni-trier.de>
2025-11-20 11:03:58 +0000

    Remove baseline script for finetuning configurations

--- experiment/01-finetune/baseline.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo-train --config ../../configs/qwen3-4b-ger-history.yaml
-echo-train --config ../../configs/qwen3-4b-eng-history.yaml
-echo-train --config ../../configs/qwen3-4b-lux-history.yaml
-echo-train --config ../../configs/qwen3-4b-mix-history.yaml
