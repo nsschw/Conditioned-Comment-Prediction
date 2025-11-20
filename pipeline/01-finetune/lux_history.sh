3514c14ec5f64b2ccbab2dc5a025939c209b713c
nsschw <schwager@uni-trier.de>
2025-11-20 11:03:17 +0000

    Add preprocessing scripts for bios generation and dataset creation

---
+++ pipeline/01-finetune/lux_history.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo-train --config ../../configs/llama3.1-8b-lux-history.yaml
+echo-train --config ../../configs/ministral-8b-lux-history.yaml
+echo-train --config ../../configs/qwen3-8b-lux-history.yaml
