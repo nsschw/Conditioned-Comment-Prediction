ce20982302a601bbb4888f00356f4745a702d410
nsschw <schwager@uni-trier.de>
2025-11-21 11:31:48 +0000

    Add configuration files for Llama, Ministral, and Qwen models in multiple languages; update generation scripts for correct paths

--- pipeline_twon/01-finetune/01-Ministral.sh
+++ pipeline_twon/01-finetune/01-Ministral.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo-train --config ../../configs/Ministral-8B-Instruct-2410-eng-bio-history.yaml
-echo-train --config ../../configs/Ministral-8B-Instruct-2410-ger-bio-history.yaml
-echo-train --config ../../configs/Ministral-8B-Instruct-2410-lux-bio-history.yaml
+echo-train --config ../../configs/twon/Ministral-8B-Instruct-2410-eng-bio-history.yaml
+echo-train --config ../../configs/twon/Ministral-8B-Instruct-2410-ger-bio-history.yaml
+echo-train --config ../../configs/twon/Ministral-8B-Instruct-2410-lux-bio-history.yaml
--- pipeline_wassa2026/01-finetune/lux_history.sh
+++ pipeline_wassa2026/01-finetune/lux_history.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo-train --config ../../configs/llama3.1-8b-lux-history.yaml
-echo-train --config ../../configs/ministral-8b-lux-history.yaml
-echo-train --config ../../configs/qwen3-8b-lux-history.yaml
+echo-train --config ../../configs/wassa26/Llama-3.1-8B-Instruct-lux-history.yaml
+echo-train --config ../../configs/wassa26/Ministral-8B-Instruct-2410-lux-history.yaml
+echo-train --config ../../configs/wassa26/Qwen3-8B-lux-history.yaml
